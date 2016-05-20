4d32385462dd58d1a42f214d60b1ab26a96c90f7
Friedrich Ewald <freddiemailster@gmail.com>
2016-05-20 11:06:06 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListIterationExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create list and fill it with number 1-100
+            List<int> myList = new List<int>();
+            for (int i = 0; i < 100; i++)
+            {
+                myList.Add(i);
+            }
+
+            // Define some upper boundary
+            int upperBoundary = 20;
+
+            foreach (var i in myList)
+            {
+                if (i > upperBoundary)
+                {
+                    myList.Remove(i);
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
